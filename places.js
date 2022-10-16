d39a186161ea35f74e04d204fa3396586fd45c41
Emmanuel-Kipngetich <emmanuel.kipngetich19@students.dkut.ac.ke>
2022-10-16 16:59:30 +0300

    New Coordinates

--- places.js
+++ places.js
@@ -1,13 +1,13 @@
 const loadPlaces = function (coords) {
     // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
-    const method = 'api';
+   // const method = 'api';
 
     const PLACES = [
         {
             name: "Your place name",
             location: {
-                lat: 0, // add here latitude if using static data
-                lng: 0, // add here longitude if using static data
+                lat: -0.398338,  // add here latitude if using static data
+                lng: 36.960885, // add here longitude if using static data
             }
         },
     ];
